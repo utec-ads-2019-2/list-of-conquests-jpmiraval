e9f839eeeb6f6c273d52e7b0200e229acde141a2
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2019-08-22 18:51:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <iterator>
+#include <map>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main(){
+    string listaPaises;
+    map <string, int> mapPaises;
+    int numeroLineas;
+    string nombre;
+    string pais;
+    cin >> numeroLineas;
+    for(int i = 0; i<numeroLineas; i++){
+        cin >> pais;
+        getline(cin, nombre);
+        mapPaises[pais]++;
+    }
+
+    for(map<string, int>::const_iterator it = mapPaises.begin();
+    it != mapPaises.end(); ++it)
+    {
+        cout<<it->first<< " " << it->second<< "\n";
+    }
     return 0;
 }
